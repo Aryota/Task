5fa0f9ea4b31ac901d149c206aa3e16c633ffc6d
ARyota <marimowakame@gmail.com>
2019-10-28 19:57:47 +0900

    タスク一覧表示

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,29 @@
 class TasksController < ApplicationController
   def index
+    @tasks =Task.all
   end
 
   def show
+    @task =Task.find(params[:id])
   end
 
   def new
-    @user = User.new
+    @task = Task.new
   end
 
   def edit
   end
+
   def create
-    task =User.new(task_params)
+    task =Task.new(task_params)
     task.save!
-    redirect_to tasks_url, notice: "タスクを「#{task.name}」登録"
+    redirect_to tasks_url, notice: "タスクを「#{task._name}」登録"
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:_name, :_description)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_104536) do
+ActiveRecord::Schema.define(version: 2019_10_28_084136) do
 
   create_table "taskins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,8 +18,16 @@ ActiveRecord::Schema.define(version: 2019_10_26_104536) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "_name"
+    t.text "_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "_name"
+    t.text "_desctiption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
