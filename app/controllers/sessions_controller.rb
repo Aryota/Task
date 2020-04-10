0241c9c642d7b4bea6fe00b653f2680958f192a3
Aryota <marimowakame@gmail.com>
2020-04-10 20:31:39 +0900

    session問題

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email] )
 
     if user&.authenticate(session_params[:password] )
-      # わかってあえてやっているのであれば問題ないのですが(公開されない前提で)、
-      # sessionで管理するこのやり方だとセキュリティ的に危険なので
-      # ライブラリ使うようにした方が良いです
       session[:user_id] = user.id
       redirect_to root_url, notice: 'logged'
     else
